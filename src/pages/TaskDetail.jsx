f6ec35d7f649a6e5cdb17bc118c9e231f65e04e0
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-05-13 11:16:16 +0200

    milestone 9

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../contexts/GlobalContext";
+import Modal from "../components/Modal";
 
 export default function TaskDetail() {
     const { id } = useParams();
@@ -8,6 +9,8 @@ export default function TaskDetail() {
     const { tasks, removeTask } = useContext(GlobalContext);
 
     const task = tasks.find(task => task.id === parseInt(id));
+
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
 
     if (!task) {
         return <h2>Nessuna Task corrispondente a questo id</h2>
@@ -32,9 +35,17 @@ export default function TaskDetail() {
                 <p><strong>Descrizione: </strong>{task.description}</p>
                 <p><strong>Stato: </strong>{task.status}</p>
                 <p><strong>Creata il: </strong>{new Date(task.createdAt).toLocaleDateString()}</p>
-                <button onClick={handleDelete}>
+                <button onClick={() => setShowDeleteModal(true)}>
                     Elimina task
                 </button>
+                <Modal
+                    title="Conferma eliminazione task"
+                    content={<p>Confermi di voler eliminare questa task?</p>}
+                    show={showDeleteModal}
+                    onClose={() => setShowDeleteModal(false)}
+                    onConfirm={handleDelete}
+                    confirmText="Elimina"
+                />
             </div>
         </>
     )
